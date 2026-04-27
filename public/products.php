537556714ec279c69591aa4f35c1105d8d1369c6
oliverr009 <oliverrnyonga@gmail.com>
2026-04-28 00:37:47 +0300

    Fix header order and add OPTIONS request handling

--- public/products.php
+++ public/products.php
@@ -1,9 +1,14 @@
 <?php
 
-header('Content-Type: application/json');
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type');
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Content-Type: application/json");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
 
 echo json_encode([
     ["name"=>"HP EliteBook 840 G6","price"=>45000],
