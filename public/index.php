eb56ea83e30bcd56b015b86f0ec76b1b928f4232
oliverr009 <oliverrnyonga@gmail.com>
2026-04-27 23:39:14 +0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,18 +1,8 @@
 <?php
 
-use Illuminate\Foundation\Application;
-use Illuminate\Http\Request;
+header('Content-Type: application/json');
 
-define('LARAVEL_START', microtime(true));
-
-require __DIR__.'/../vendor/autoload.php';
-
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-$response = $kernel->handle(
-    $request = Request::capture()
-)->send();
-
-$kernel->terminate($request, $response);
+echo json_encode([
+    "status" => "Backend is working",
+    "app" => "Coarse Technologies API"
+]);
