931ed60df31d79da35d613bed9a23d4d48109ab4
oliverr009 <oliverrnyonga@gmail.com>
2026-04-28 00:18:34 +0300

    Refactor product array syntax in products.php

--- public/products.php
+++ public/products.php
@@ -1,18 +1,12 @@
 <?php
 
 header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
 
 echo json_encode([
-    [
-        "name" => "HP EliteBook 840 G6",
-        "price" => 45000
-    ],
-    [
-        "name" => "Lenovo ThinkPad T480",
-        "price" => 52000
-    ],
-    [
-        "name" => "POS System Bundle",
-        "price" => 85000
-    ]
+    ["name"=>"HP EliteBook 840 G6","price"=>45000],
+    ["name"=>"Lenovo ThinkPad T480","price"=>52000],
+    ["name"=>"POS System Bundle","price"=>85000]
 ]);
